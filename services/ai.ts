13716cd3c0cd785141b1636be38132eb00ca0dd9
Alex Fitterling <info@sp33c.tech>
2025-04-03 00:04:29 +0200

    removed keys

--- services/ai.ts
+++ services/ai.ts
@@ -4,7 +4,5 @@ import { Resource } from "sst";
 export default function ai() {
     return new OpenAI({
         apiKey: Resource.OPEN_AI_SECRET.value,
-        organisation: "org-SrHhs2i4xnQZ37GqS98CTbVI",
-        project: "proj_cij9i6MKiwhQPpNIRyaVSDtj"
     } as any);
 }
