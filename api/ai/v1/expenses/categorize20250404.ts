faec69dbda251377f3f77f01ce3819bdbb8be8ad
Alex Fitterling <info@sp33c.tech>
2025-04-04 22:17:09 +0200

    Update categorize20250404.ts

--- api/ai/v1/expenses/categorize20250404.ts
+++ api/ai/v1/expenses/categorize20250404.ts
@@ -2,45 +2,6 @@ import _ from "lodash";
 import OpenAI from "openai";
 import { Resource } from "sst";
 
-/*
-
-response = client.responses.create(
-    model="gpt-4o-2024-08-06",
-    input=[
-        {"role": "system", "content": "Extract the event information."},
-        {"role": "user", "content": "Alice and Bob are going to a science fair on Friday."}
-    ],
-    text={
-        "format": {
-            "type": "json_schema",
-            "name": "calendar_event",
-            "schema": {
-                "type": "object",
-                "properties": {
-                    "name": {
-                        "type": "string"
-                    },
-                    "date": {
-                        "type": "string"
-                    },
-                    "participants": {
-                        "type": "array", 
-                        "items": {
-                            "type": "string"
-                        }
-                    },
-                },
-                "required": ["name", "date", "participants"],
-                "additionalProperties": False
-            },
-            "strict": True
-        }
-    }
-)
-
-event = json.loads(response.output_text)
-*/
-
 export async function handler(event) {
     const ai = new OpenAI({
         apiKey: Resource.OPEN_AI_SECRET.value,
